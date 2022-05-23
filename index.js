77d3faa40c34a71c5c6c1c03c311eeae0dcc2bc5
Daniela Rodriguez <rodriguezberriod@gmail.com>
2022-05-23 14:13:27 -0500

    backend en git 1

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { getConnection } = require('./db/DBConnection-mongo');
+const tipoEquipo = require('./models/TipoEquipo');
+const estadoEquipo = require('./models/EstadoEquipo');
+const marca = require('./models/Marca');
+const usuario = require('./models/Usuario');
+const inventario = require('./models/Inventario');
+
+
+const application = express();
+const port = 4000;
+
+
+getConnection();
+
+application.use(express.json());
+
+application.use('/usuario', require('./router/usuario'));
+application.use('/estado-equipo', require('./router/estadoEquipo'));
+application.use('/tipo-equipo', require('./router/tipoEquipo'));
+application.use('/marca', require('./router/marca'));
+application.use('/inventario', require('./router/inventario'));
+
+
+application.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
---
+++ router/usuario.js
@@ -0,0 +1,82 @@
+const {Router} = require('express');
+const { validateUser } = require('../helpers/validate-user');
+const router = Router();
+const Usuario = require('../models/Usuario');
+
+router.post('/', async function(req, res){
+
+    try{
+        const validations = validateUser(req);
+
+        if(validations.length > 0){
+            return res.status(400).send(validations);
+        }
+
+        console.log('It has been received',req.body);
+
+        const existeUsuario = await Usuario.findOne({email: req.body.email});
+        console.log(existeUsuario);
+        if(existeUsuario){
+            return res.status(400).send('Email already exists');
+        }
+    
+    let usuario = new Usuario();
+    usuario.nombre = req.body.nombre;
+    usuario.email = req.body.email;
+    usuario.estado = req.body.estado;
+    usuario.fechaCreacion = new Date();
+    usuario.fechaActualizacion = new Date();
+
+    usuario = await usuario.save();
+
+    res.send(usuario);
+
+    }catch(error){
+        console.log(error);
+        res.status(500).send('an error occurred with server');
+    }
+    
+});
+
+router.get('/', async function(req, res){
+    try{
+        const usuarios = await Usuario.find();
+        res.send(usuarios);
+    }catch(error){
+        console.log(error);
+        res.status(500).send('an error occurred with server');
+    }
+});
+
+router.put('/:usuarioId', async function(req, res){
+    try{
+
+        let usuario = await Usuario.findById(req.params.usuarioId);
+        if(!usuario){
+            return res.status(400).send('does not exists user');
+        }
+
+        const existeUsuario = await Usuario
+        .findOne({email: req.body.email, _id:{$ne: usuario._id}});
+        console.log(existeUsuario);
+        if(existeUsuario){
+            return res.status(400).send('Email already exists');
+        }
+        
+        usuario.nombre = req.body.nombre;
+        usuario.email = req.body.email;
+        usuario.estado = req.body.estado;
+        usuario.fechaCreacion = new Date();
+        usuario.fechaActualizacion = new Date();
+    
+        usuario = await usuario.save();
+    
+        res.send(usuario);
+
+    }catch(error){
+        console.log(error);
+        res.status(500).send('an error occurred with server');
+    }
+});
+
+module.exports = router;
