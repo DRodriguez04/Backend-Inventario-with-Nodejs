304c466ad3245a366c05e92306bd00dd529f586f
Daniela Rodriguez <rodriguezberriod@gmail.com>
2022-06-06 18:52:42 -0500

    operador añadido

--- helpers/validate-inventory.js
+++ helpers/validate-inventory.js
@@ -3,43 +3,43 @@ const req = require("express/lib/request");
 const validateInventory = (req) => {
     const validaciones = [];
 
-    if(req.body.serial){
+    if(!req.body.serial){
         validaciones.push('required serial');
     }
 
-    if(req.body.modelo){
+    if(!req.body.modelo){
         validaciones.push('required model');
     }
 
-    if(req.body.descripcion){
+    if(!req.body.descripcion){
         validaciones.push('required description');
     }
 
-    if(req.body.foto){
+    if(!req.body.foto){
         validaciones.push('required photo');
     }
 
-    if(req.body.precio){
+    if(!req.body.precio){
         validaciones.push('required price');
     }
 
-    if(req.body.fechaCompra){
+    if(!req.body.fechaCompra){
         validaciones.push('required purchase date');
     }
 
-    if(req.body.usuario){
+    if(!req.body.usuario){
         validaciones.push('required user');
     }
 
-    if(req.body.marca){
+    if(!req.body.marca){
         validaciones.push('required mark');
     }
 
-    if(req.body.tipoEquipo){
+    if(!req.body.tipoEquipo){
         validaciones.push('required type of equipment');
     }
 
-    if(req.body.estadoEquipo){
+    if(!req.body.estadoEquipo){
         validaciones.push('required equipment status');
     }
 
